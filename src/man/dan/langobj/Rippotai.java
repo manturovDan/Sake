83e424c3207f16ee07f90dab4a69cfbfa3b980e1
manturovDan <manturov.dan@mail.ru>
2020-05-10 12:35:01 +0300

    kabe start

---
+++ src/man/dan/langobj/Rippotai.java
@@ -0,0 +1,17 @@
+package man.dan.langobj;
+
+public class Rippotai implements SakeObj {
+    protected int x;
+    protected int y;
+    protected int z;
+    protected boolean kabe; //wall
+
+    public Rippotai(int _x, int _y, int _z, boolean _kabe) {
+        x = _x;
+        y = _y;
+        z = _z;
+        kabe = _kabe;
+    }
+
+
+}
