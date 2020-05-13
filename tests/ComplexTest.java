a499156d4ca2be79305b59efbfd077520ef16854
manturovDan <manturov.dan@mail.ru>
2020-05-13 22:10:35 +0300

    interpreter streams

---
+++ tests/ComplexTest.java
@@ -0,0 +1,45 @@
+import man.dan.interpreter.Interpreter;
+import man.dan.langobj.Countable;
+import man.dan.memory.AreaVis;
+import org.junit.Test;
+
+import java.io.*;
+
+import static org.junit.Assert.assertEquals;
+
+public class ComplexTest {
+    protected void executeWithClear(String initialString, OutputStream progOut, OutputStream progErr) throws IOException {
+        InputStream progIn = new ByteArrayInputStream(initialString.getBytes());
+        Interpreter interpreter;
+        interpreter = new Interpreter(progIn, progOut, progErr);
+        interpreter.run();
+    }
+
+    @Test
+    public void simpleCalc() throws Exception {
+        String initialString =  "seisu bz = -xAF;\n" +
+                                "senden bz;\n" +
+                                "hairetsu la = {4, 2};\n" +
+                                "la[2, 1] = [1, 2, 0, shinri];\n" +
+                                "senden la;\n" +
+                                "senden la[2, 1] => Z;\n" +
+                                "la[2, 1] => Y = 5;\n" +
+                                "senden la[2, 1] => Y;\n" +
+                                "senden la[2, 1];\n" +
+                                "rippotai kb = [0, 0, 0, 5];\n" +
+                                "la[2, 0] = kb;\n" +
+                                "kb => X = 1;\n" +
+                                "senden la;\n" +
+                                "senden kb;";
+
+        OutputStream progOut = new ByteArrayOutputStream();
+        OutputStream progErr = new ByteArrayOutputStream();
+
+        executeWithClear(initialString, progOut, progErr);
+
+        //assertEquals(((Countable)memory.getValByPtr("c1")).getValue(), 121);
+        //assertEquals(((Countable)memory.getValByPtr("b")).getValue(), 152);
+
+        System.out.println(progOut.toString());
+    }
+}
