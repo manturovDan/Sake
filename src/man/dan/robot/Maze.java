44c63d4d2597d067a4f2e90c82ea680c472e7971
manturovDan <manturov.dan@mail.ru>
2020-05-15 12:52:13 +0300

    maze creating

--- src/man/dan/robot/Maze.java
+++ src/man/dan/robot/Maze.java
@@ -1,13 +1,19 @@
 package man.dan.robot;
 
+import io.vavr.Tuple3;
+
+import javax.management.modelmbean.XMLParseException;
+import javax.xml.namespace.QName;
 import javax.xml.stream.XMLEventReader;
 import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.events.StartElement;
 import javax.xml.stream.events.XMLEvent;
 import java.io.InputStream;
+import java.util.HashSet;
 
 public class Maze {
     protected InputStream mazeStreamXML;
+    protected HashSet<Passage> way;
 
     public Maze(InputStream ms) throws Exception {
         mazeStreamXML = ms;
@@ -15,14 +21,53 @@ public class Maze {
         XMLInputFactory xmlIF = XMLInputFactory.newInstance();
         XMLEventReader reader = xmlIF.createXMLEventReader(mazeStreamXML);
 
+        boolean inMaze = false;
+        boolean inSpaces = false;
+        Tuple3<Integer, Integer, Integer> robot = null;
+
         while (reader.hasNext()) {
             XMLEvent nextEvent = reader.nextEvent();
             if (nextEvent.isStartElement()) {
                 StartElement startElement = nextEvent.asStartElement();
-                if (startElement.getName().getLocalPart().equals("maze")) {
-                    System.out.println("MAZE");
+                if (startElement.getName().getLocalPart().equals("maze"))
+                    inMaze = true;
+                else if (startElement.getName().getLocalPart().equals("robot") && inMaze) {
+                    if (robot != null)
+                        throw new XMLParseException("there must be only one robot");
+
+                    robot = new Tuple3<>(   Integer.parseInt(startElement.getAttributeByName(new QName("X")).getValue()),
+                            Integer.parseInt(startElement.getAttributeByName(new QName("Y")).getValue()),
+                            Integer.parseInt(startElement.getAttributeByName(new QName("Z")).getValue()));
+
+                    System.out.println("ROBOT");
                 }
+                else if (startElement.getName().getLocalPart().equals("spaces") && inMaze) {
+                    inSpaces = true;
+                    way = new HashSet<>();
+                } else if (inMaze && inSpaces) {
+                    if (startElement.getName().getLocalPart().equals("passage")) {
+                        int x = getIntFromArg(startElement, "X");
+                        int y = getIntFromArg(startElement, "Y");
+                        int z = getIntFromArg(startElement, "Z");
+                        way.add(new Passage(x, y, z));
+                        System.out.println("PASSAGE");
+                    }
+                    else if (startElement.getName().getLocalPart().equals("portal")) {
+                        way.add(new Passage(    Integer.parseInt(startElement.getAttributeByName(new QName("X")).getValue()),
+                                                Integer.parseInt(startElement.getAttributeByName(new QName("Y")).getValue()),
+                                                Integer.parseInt(startElement.getAttributeByName(new QName("Z")).getValue()), true));
+                        System.out.println("PORTAL");
+                    }
+                    else
+                        throw new XMLParseException("wrong XML tag");
+                }
+                else
+                    throw new XMLParseException("wrong XML structure");
             }
         }
+    }
+
+    protected Integer getIntFromArg(StartElement startElement, String arg) {
+        return Integer.parseInt(startElement.getAttributeByName(new QName(arg)).getValue());
     }
 }
--- src/man/dan/robot/Passage.java
+++ src/man/dan/robot/Passage.java
@@ -1,4 +1,21 @@
 package man.dan.robot;
 
 public class Passage {
+    protected int x;
+    protected int y;
+    protected int z;
+    protected boolean portal;
+
+    public Passage(int _x, int _y, int _z) {
+        x = _x;
+        y = _y;
+        z = _z;
+    }
+
+    public Passage(int _x, int _y, int _z, boolean _p) {
+        x = _x;
+        y = _y;
+        z = _z;
+        portal = _p;
+    }
 }
