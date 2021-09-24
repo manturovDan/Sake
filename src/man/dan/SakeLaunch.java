2b8a6e830e8df55ea9790e9d19705edcc74e7210
manturovDan <manturov.dan@mail.ru>
2021-09-24 19:17:45 +0300

    new branch bugfix1

--- src/man/dan/SakeLaunch.java
+++ src/man/dan/SakeLaunch.java
@@ -8,7 +8,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
-//comment 2
+//comment 3
 public class SakeLaunch {
     public static void main(String[] args) {
         InputStream progIn = null;
